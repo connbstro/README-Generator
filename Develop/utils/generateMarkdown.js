3b11646b5b4064ba0a709d028dcc3b7fd3028764
Connor S <constroh@gmail.com>
2023-02-14 14:05:22 -0600

    renderLicenseBadge

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,21 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge = "";
+  if (license === "MIT") {
+    badge =
+      "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+  } else if (license === "GPL") {
+    badge =
+      "![License: GPL](https://img.shields.io/badge/License-GPLv3-blue.svg)";
+  } else if (license === "Apache") {
+    badge =
+      "![License: Apache](https://img.shields.io/badge/License-Apache_2.0-blue.svg)";
+  } else {
+    badge = "";
+  }
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string

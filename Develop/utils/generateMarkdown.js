9403ce8c66d348d8b8de4dd4e740ab779e5c7016
Connor S <constroh@gmail.com>
2023-02-14 16:47:45 -0600

    added missing renderingLicense function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -35,7 +35,14 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSection = "";
+  if (license === "None") {
+    licenseSection = "";
+  } else {
+    licenseSection = "License: "
+  } return licenseSection;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -51,7 +58,7 @@ function generateMarkdown(data) {
 
   ## **Table of Contents**
   <details>
-  <summary>Click to expand page</summary>
+  <summary>Click to expand contents</summary>
 
   ### [Build](#Build)
   ### [Description](#Description)
